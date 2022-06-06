4d1b836f0912a493e4751623f602cebd96f5dcdc
skarifmahammad <arifmahammad2018@gmail.com>
2022-06-06 12:31:29 +0530

    Add Totate Matrix

--- Array_2/Rotate_Matrix.java
+++ Array_2/Rotate_Matrix.java
@@ -1,21 +1,21 @@
 package Array_2;
 
 class Solution {
-    public void rotate(int[][] ans) {
-        for (int i = 0; i < ans.length; i++) {
-            for (int j = i; j < ans[0].length; j++) {
+    public void rotate(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
                 int temp = 0;
-                temp = ans[i][j];
-                ans[i][j] = ans[j][i];
-                ans[j][i] = temp;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
-        for (int i = 0; i < ans.length; i++) {
-            for (int j = 0; j < ans.length / 2; j++) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
                 int temp = 0;
-                temp = ans[i][j];
-                ans[i][j] = ans[i][ans.length - 1 - j];
-                ans[i][ans.length - 1 - j] = temp;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
             }
         }
     }
