dba883a24ac6049f7b733f0870436f9bb3799c54
skarifmahammad <arifmahammad2018@gmail.com>
2022-06-08 23:58:34 +0530

    Find_Duplicate_and_Double

---
+++ Array_2/Find_duplicate_double.java
@@ -0,0 +1,50 @@
+public class Find_duplicate_double {
+    public static void main(String[] args) {
+
+    }
+}
+
+public class myclass {
+    int[] findTwoElement(int arr[], int n) {
+        // code here
+        // int min=1;
+        // Arrays.sort(arr);
+        // int[] ans = new int[2];
+        // for(int i=1; i<n; i++){
+        // if(arr[i-1]==arr[i]){
+        // ans[0] = arr[i];
+        // }
+        // }
+        // Set<Integer> set = new HashSet<Integer>();
+        // for(int j=0; j<n; j++){
+        // set.add(arr[j]);
+        // }
+        // for(int j=0; j<n; j++){
+        // if(!set.contains(min)){
+        // ans[1] = min;
+        // break;
+        // }
+        // else
+        // min++;
+        // }
+        // return ans;
+
+        int[] ans = new int[2];
+        int[] ppp = new int[n];
+        // for(int i=0; i<n; i++){
+        // ppp[arr[i]]++;
+        // }
+        for (int i : arr) {
+            ppp[i - 1]++;
+        }
+        for (int j = 1; j <= ppp.length; j++) {
+            if (ppp[j - 1] > 1) {
+                ans[0] = j;
+            }
+            if (ppp[j - 1] == 0) {
+                ans[1] = j;
+            }
+        }
+        return ans;
+    }
+}
