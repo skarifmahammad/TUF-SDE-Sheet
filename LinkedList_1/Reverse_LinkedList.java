39eef3bbca346a73b33dc89e70bd5ca5ef4a074f
skarifmahammad <arifmahammad2018@gmail.com>
2022-06-30 19:52:17 +0530

    LinkedList_I

---
+++ LinkedList_1/Reverse_LinkedList.java
@@ -0,0 +1,34 @@
+package LinkedList_1;
+
+import java.util.*;
+
+public class Reverse_LinkedList {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+}
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
